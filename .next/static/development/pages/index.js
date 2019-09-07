81d4c0282521ac0aef81bd6fba67cb3bbaa85887
szy1000 <1538078053@qq.com>
2019-09-07 21:39:20 +0800

    init next

---
+++ .next/static/development/pages/index.js
@@ -0,0 +1,87 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/_next@9.0.5@next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fshenzhiyong%2FDocuments%2Fgithub%2Fnext%2Fpages%2Findex.jsx!./":
+/*!***********************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/_next@9.0.5@next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fshenzhiyong%2FDocuments%2Fgithub%2Fnext%2Fpages%2Findex.jsx ***!
+  \***********************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.jsx */ "./pages/index.jsx")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.jsx */ "./pages/index.jsx", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.jsx */ "./pages/index.jsx")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/_react@16.9.0@react/index.js":
+/*!*********************************************************************************************************!*\
+  !*** delegated ./node_modules/_react@16.9.0@react/index.js from dll-reference dll_90ea89e3e5cb602517af ***!
+  \*********************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_90ea89e3e5cb602517af */ "dll-reference dll_90ea89e3e5cb602517af"))("./node_modules/_react@16.9.0@react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.jsx":
+/*!*************************!*\
+  !*** ./pages/index.jsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/_react@16.9.0@react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/shenzhiyong/Documents/github/next/pages/index.jsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    },
+    __self: this
+  }, "index");
+});
+
+/***/ }),
+
+/***/ 0:
+/*!***************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fshenzhiyong%2FDocuments%2Fgithub%2Fnext%2Fpages%2Findex.jsx ***!
+  \***************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fshenzhiyong%2FDocuments%2Fgithub%2Fnext%2Fpages%2Findex.jsx! */"./node_modules/_next@9.0.5@next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fshenzhiyong%2FDocuments%2Fgithub%2Fnext%2Fpages%2Findex.jsx!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_90ea89e3e5cb602517af":
+/*!*******************************************!*\
+  !*** external "dll_90ea89e3e5cb602517af" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_90ea89e3e5cb602517af;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
