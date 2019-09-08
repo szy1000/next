12158460ed5575e1adffdaf43aab45c57a36d702
szy1000 <1538078053@qq.com>
2019-09-08 11:10:38 +0800

    get some koa skill

--- service.js
+++ service.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,15 +8,43 @@ const app = next({ dev })
 
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-    const server = new Koa()
+// app.prepare().then(() => {
+//     const server = new Koa()
+//
+//     server.use(async (ctx, next) => {
+//         await handle(ctx.req, ctx.res)
+//         ctx.respond = false
+//     })
+//
+//     server.listen(3000, () => {
+//         console.log('listening on 3000')
+//     })
+// })
 
-    server.use(async (ctx, next) => {
-        await handle(ctx.req, ctx.res)
-        ctx.respond = false
-    })
+const server = new Koa()
 
-    server.listen(3000, () => {
-        console.log('listening on 3000')
-    })
+const router = new Router()
+
+router.get('/test', (ctx) => {
+  // ctx.body = 'rouer'
+  ctx.body = {success: true}
+  // ctx.set('Content-Type', 'application/json')
+})
+
+
+server.use(router.routes())
+
+// server.use(async (ctx, next) => {
+//   // const path = ctx.path
+//   const {path, method} = ctx
+//   ctx.body = `<h1>Koa</h1> ${path} , method: ${method}`
+//   await next()
+// })
+
+// server.use(async (ctx, next) => {
+//     ctx.body = '<h1>Koa2</h1>'
+// })
+
+server.listen(3000, () => {
+  console.log('listening on 3000')
 })
