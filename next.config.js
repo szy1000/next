e23e20c0beda88efa6d386f2030caabeaa68fc07
szy1000 <1538078053@qq.com>
2019-09-10 23:08:25 +0800

    add some config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,24 @@ if(typeof require !== 'undefined') {
   require.extensions['.css'] = file => {}
 }
 
+config = {
+  distDir: 'dist',
+  generateEtags: true,  //nginx使用了etag 设置为false
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  pageExtensions: ['jsx', 'js'],
+
+  // generateBuildId: async () => {
+  //   if
+  //   return null
+  // }
+
+  webpack(config, options) {
+    return config
+  },
+
+  
+}
 module.exports = withCss({})
